e2e513d79b05b20d874c89f5394df5b45610457a
Rreuti24 <ricarda.reutimann@gmail.com>
2019-10-04 09:50:45 +0200

    PaintableScript

--- EmotionInVR/Assets/paintable.cs
+++ EmotionInVR/Assets/paintable.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class paintable : MonoBehaviour {
 
+    public GameObject Brush;
+    public float BrushSize = 0.1f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,17 @@ public class paintable : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(Ray, out hit))
+            {
+                var go = Instantiate(Brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+                go.transform.localScale = Vector3.one * BrushSize;
+            }
+        }
 		
 	}
 }
